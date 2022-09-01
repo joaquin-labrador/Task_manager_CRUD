f395bb5ccea66fb4921609700df94c3d4de4804d
joaquin-labrador <joaquinlabrador1010@gmail.com>
2022-09-01 19:49:29 -0300

    Api V1.0 finished 🎆

--- src/controller/tasks.controller.js
+++ src/controller/tasks.controller.js
@@ -52,10 +52,15 @@ const updateTask = async (req, res) => {
   }
 };
 
+
 const updateTaskPost = async (req, res) => {
+  try{
   const { id } = req.params;
   await Task.findByIdAndUpdate(id, req.body);
   res.redirect("/apiTask/v1.0");
+  }catch(error){
+    return res.render("error", { errorMessage: error.message });
+  }
 };
 
 export default {
